3a7f78e858d9e2ead160f5949577cfdaa11307c3
songJingZhe <1529703426@qq.com>
2022-04-04 17:32:51 +0800

    111

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -12,14 +12,14 @@ import { mapGetters } from 'vuex'
 
 export default {
   name: 'Dashboard',
-  
+
   data(){
     return {
       goods:[
         {
           goodsType:"饮料",
           goodsArr:[
-            {
+            {  
               Name:'可口可乐500ml',
               imgUrl:'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.defanli.com%2Fi3%2F2208182652164%2FO1CN01wtAPta1Rr9lTVqfbX_%21%212208182652164.jpg_q90.jpg&refer=http%3A%2F%2Fimg.defanli.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1651654933&t=67682a4b831f872f1eb8853df433a4a6',
               price:'3'
